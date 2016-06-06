f50b420ee3e39ef85ec9e5a2745c1d0c35c3485f
Mateo Carvajal <mateocarvajal@MacBook-Pro-Mateo-619.local>
2016-06-05 19:01:06 -0500

    bookmarks get and post

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.3.1'
 
 gem 'cuba', '~> 3.6.0'
 gem 'mongoid', '~> 5.1.0'
+gem 'json', '~> 1.8'
 
 group :test do
   gem 'rubocop', '~> 0.40.0', require: false
--- api/bookmarks/routes.rb
+++ api/bookmarks/routes.rb
@@ -1,4 +1,5 @@
 require './data/repositories/bookmark'
+require 'json'
 
 module API
   # Bookmark defines the routes for the bookmark resource.
@@ -8,13 +9,23 @@ module API
         on ':id' do |id|
           on root do
             begin
-              res.write Repositories::Bookmark.find id
+              res.write Repositories::Bookmark.find(id).to_json
             rescue Mongoid::Errors::DocumentNotFound
               res.status = 404
               res.write 'Document not found.'
             end
           end
         end
+      end
+      on post do
+        bookmark = JSON.parse(req.body.read);
+        if !bookmark['url'] then
+          res.status = 404
+          res.write 'Url is mandatory'
+        end
+        puts bookmark['url']
+        #TODO add to the loged user
+        res.write Repositories::Bookmark.create(url: bookmark['url'], name: bookmark['name']).to_json
       end
     end
   end
