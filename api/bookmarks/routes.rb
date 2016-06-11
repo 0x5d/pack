cd5782b7b91e1d3506cb2b55657b0c759398e3d7
Mateo Carvajal <mateocarvajal@MacBook-Pro-Mateo-619.local>
2016-06-11 14:56:48 -0500

    code review fixes

--- api/bookmarks/routes.rb
+++ api/bookmarks/routes.rb
@@ -17,6 +17,7 @@ module API
           end
         end
       end
+      
       on post do
         on root do
           bookmark = JSON.parse(req.body.read)
--- test/api/bookmarks/routes.rb
+++ test/api/bookmarks/routes.rb
@@ -2,6 +2,7 @@ require 'cuba/test'
 require 'json'
 
 require './app'
+require './data/repositories/bookmark'
 
 setup do
   { post: { method: 'post', content_type: 'application/json' } }
@@ -26,20 +27,10 @@ scope do
     assert defined?(last_response.body['name'])
   end
 
-  test 'creates and gets a bookmark' do |config|
-    bookmark = {
-      url: 'es.stackoverflow.com/',
-      name: 'stackoverflow home page'
-    }.to_json
-
-    request '/bookmarks', config[:post].merge(input: bookmark)
-    assert_equal 200, last_response.status
-    assert defined?(last_response.body['_id'])
-    assert defined?(last_response.body['url'])
-    assert defined?(last_response.body['name'])
-    post_response = JSON.parse(last_response.body)
-
-    get '/bookmarks/' + post_response['_id']['$oid']
+  test 'gets a bookmark' do |config|
+    bookmark = Repositories::Bookmark
+      .create(url: 'es.stackoverflow.com/', name: 'stackoverflow home page')
+    get '/bookmarks/' + bookmark['_id']
     assert_equal 200, last_response.status
     assert defined?(last_response.body['_id'])
     assert defined?(last_response.body['url'])
@@ -48,6 +39,6 @@ scope do
     get_response = JSON.parse(last_response.body)
     assert_equal 'es.stackoverflow.com/', get_response['url']
     assert_equal 'stackoverflow home page', get_response['name']
-    assert_equal post_response['_id']['$oid'], get_response['_id']['$oid']
+    assert_equal bookmark['_id'], BSON::ObjectId.from_string(get_response['_id']['$oid'])
   end
 end
