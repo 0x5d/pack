f579ac9e265af794375ea22e6b32dfd24fae0a84
David Castillo <castillobgr@gmail.com>
2016-06-11 13:57:59 -0500

    Refactor Pack and add tests

--- api/packs/routes.rb
+++ api/packs/routes.rb
@@ -1,11 +1,11 @@
-require './data/repositories/bookmark'
+require './data/repositories/pack'
 
 module API
   # Bookmark defines the routes for the bookmark resource.
   class Pack < Cuba
     define do
-      on get do
-        on ':id' do |id|
+      on ':id' do |id|
+        on get do
           on root do
             begin
               res.write Repositories::Pack.find id
@@ -18,8 +18,13 @@ module API
       end
 
       on post do
-        pack = JSON.parse(req.body.read);
-        res.write Repositories::Bookmark.create(pack).to_json
+        pack = JSON.parse(req.body.read)
+        begin
+          res.write Repositories::Pack.create!(pack).to_json
+        rescue Mongoid::Errors::Validations => error
+          res.status = 400
+          res.write error.message
+        end
       end
     end
   end
